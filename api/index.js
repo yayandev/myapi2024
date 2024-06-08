e21595d975d6e1bfcb1ef4aef1e90cb0a4b8c6ab
yayandev <yayanfathurohman20@gmail.com>
2024-06-09 05:07:32 +0700

    add: setup vercel

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import router from "./routes/index.js";
+import router from "../routes/index.js";
 
 dotenv.config();
 const app = express();
