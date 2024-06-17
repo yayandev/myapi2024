f395112978619c66fc84ad119a45b292a6f8d89d
yayandev <yayanfathurohman20@gmail.com>
2024-06-18 05:23:53 +0700

    add rate limit

--- index.js
+++ index.js
@@ -3,9 +3,23 @@ import cors from "cors";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import router from "./routes/index.js";
-
+import rateLimit from "express-rate-limit";
 dotenv.config();
+
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 60,
+  message: {
+    status: 429,
+    message: "Terlalu banyak permintaan dari IP ini, harap coba lagi nanti.",
+  },
+  headers: true,
+  statusCode: 429,
+});
+
 const app = express();
+
+app.use(limiter);
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
