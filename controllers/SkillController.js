949e982d5a6533ba750fa5953aadb4d0856d0d20
yayandev <yayanfathurohman20@gmail.com>
2024-06-21 09:12:10 +0700

    add pagination skills

--- controllers/SkillController.js
+++ controllers/SkillController.js
@@ -184,6 +184,26 @@ export const getSkillByIdPublic = async (req, res) => {
 export const getSkillByAuthorId = async (req, res) => {
   try {
     const userId = req.user.userId;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const page = parseInt(req.query.page, 10) || 1;
+
+    if (isNaN(limit) || isNaN(page) || limit <= 0 || page <= 0) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid limit or page parameter" });
+    }
+
+    const offset = (page - 1) * limit;
+    console.log(`Limit: ${limit}, Page: ${page}, Offset: ${offset}`);
+
+    const count = await prisma.skill.count({
+      where: {
+        authorId: userId,
+      },
+    });
+    console.log(`Total Skills Count: ${count}`);
+
+    const totalPages = Math.ceil(count / limit);
 
     const skills = await prisma.skill.findMany({
       where: {
@@ -202,11 +222,23 @@ export const getSkillByAuthorId = async (req, res) => {
           },
         },
       },
+      take: limit,
+      skip: offset,
     });
 
-    res.status(200).json({ success: true, data: skills, message: "My Skills" });
+    const nextPage = page < totalPages ? page + 1 : null;
+    const prevPage = page > 1 ? page - 1 : null;
+
+    res.status(200).json({
+      success: true,
+      data: skills,
+      message: "My Skills",
+      nextPage,
+      prevPage,
+      totalPages,
+    });
   } catch (error) {
-    console.log("Error: " + error.message);
+    console.error("Error: ", error.message);
     return res.status(500).json({ success: false, message: "Server Error" });
   }
 };
